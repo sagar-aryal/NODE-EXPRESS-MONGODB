bc3836ae3f7307d20fbeb0964cff6f3ba29e6c2d
Sagar Aryal <sagar_yal92@outlook.com>
2022-02-08 19:53:31 +0200

    buffer example in nodeJs

---
+++ Exercises/7.Buffer/index.js
@@ -0,0 +1,12 @@
+/* 
+const buff = Buffer.from("Hey!");
+console.log(buff);
+console.log(buff.toString()); 
+*/
+
+/* 
+const buf = Buffer.alloc(4);
+buf.write("Hey!");
+console.log("buf");
+console.log(buf.toString());
+ */
